0fdb5c23b1df420c980427ebb91ab8c572cbe35b
Giovanni Panaro <tsrwebgl@gmail.com>
2020-04-05 14:37:49 -0400

    Solve Problem 10.

---
+++ 1-1000/1-100/1-10/pe10.dart
@@ -0,0 +1,15 @@
+import '../../../lib/prime.dart';
+
+void main() {
+  PrimeGenerator p = new PrimeGenerator();
+  int sum = 0;;
+  for(int i = 1; true; i++) {
+    int prime = p.getPrime(i);
+    if(prime < 2000000) {
+      sum += prime;
+    } else {
+      break;
+    }
+  }
+  print(sum);
+}
