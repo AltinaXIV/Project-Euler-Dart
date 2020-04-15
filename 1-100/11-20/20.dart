f812daafaeb5a5ec2adbd5b6c0614e3d1dce1b5c
Giovanni Panaro <tsrwebgl@gmail.com>
2020-04-14 20:07:58 -0400

    20

---
+++ 1-100/11-20/20.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+void main() {
+  BigInt factorial = BigInt.from(1);
+  for(int i = 100; i > 0; i--) {
+    factorial *= BigInt.from(i);
+  }
+  String factorialString = factorial.toString();
+  int sum = 0;
+  for(int i = 0; i < factorialString.length; i++) {
+    sum += int.parse(factorialString[i]);
+  }
+  print(sum);
+}
